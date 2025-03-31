db26939b114c95d74477046cae290180d5ca1140
Triveni-2526 <boppuditriveni1410@gmail.com>
2025-03-31 12:55:54 +0530

    1st change

--- EvenNum.java
+++ EvenNum.java
@@ -8,7 +8,7 @@ public class EvenNum {
 
 	public static void main(String[] args) {
 		
-		List<Integer> asList = Arrays.asList(1,3,5,6,2,12,16,18,19);
+		List<Integer> asList = Arrays.asList(1,3,5,6,2,12,16,18,19,25);
 		
 		List<Integer> collect = asList.stream().filter(i->i%2==0).collect(Collectors.toList());
 		
